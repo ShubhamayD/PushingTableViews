ea58d6101dd1c7bcd237c52171283ce0883e72a3
Shubhamay Das <shubhamd@thoughtworks.com>
2016-12-08 22:24:41 +0530

    Initial Commit

---
+++ PushingTableViews.git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PushingTableViews.git
+//
+//  Created by shubhamb on 12/8/16.
+//  Copyright © 2016 ThouhgtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PushingTableViews.gitTests/PushingTableViews_gitTests.m
@@ -0,0 +1,39 @@
+//
+//  PushingTableViews_gitTests.m
+//  PushingTableViews.gitTests
+//
+//  Created by shubhamb on 12/8/16.
+//  Copyright © 2016 ThouhgtWorks. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PushingTableViews_gitTests : XCTestCase
+
+@end
+
+@implementation PushingTableViews_gitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
